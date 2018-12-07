4ebddec7bc87c4aa04e7098503de70e5c4f2c97b
aydar0894 <aydar0894@mail.ru>
2018-12-07 21:09:58 +0300

    Strategy saver

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from api import views
 
 urlpatterns = [
-    url(r'^save_strategy$', views.save_strategy)
+    url(r'^save_strategy$', views.save_strategy),
+    url(r'^strategies_list$', views.strategies_list)
 ]
--- api/views.py
+++ api/views.py
@@ -15,15 +15,32 @@ class MatrixForm(forms.Form):
     json_representation = forms.CharField()
     ast = forms.CharField()
     user_id = forms.CharField()
-    name = forms.CharField()
+    bot_name = forms.CharField()
     frontend_graph = forms.CharField()
+
+
+@csrf_exempt
+def strategies_list(request):
+    if request.method == 'GET':
+        client = MongoClient('localhost',
+                        authSource='bitcoin')
+        db = client.bitcoin
+        strategies = db.strategies
+        result = {}
+        cursor = strategies.find({}, {'_id': 0, 'bot_name': 1, 'user_id': 1, 'ast': 1, 'frontend_graph': 1, 'json_representation': 1})
+        i = 0
+        for document in cursor:
+            result.update({str(i): document})
+            i +=1
+        return JsonResponse(result, safe=False)
+    return JsonResponse({"message": "Error"}, safe=False)
 
 @csrf_exempt
 def save_strategy(request):
 
     if request.method == 'POST':
         form = MatrixForm(request.POST)
-        # pprint(request.form)
+        print(form)
         if form.is_valid():
             client = MongoClient('localhost',
                             authSource='bitcoin')
@@ -31,11 +48,12 @@ def save_strategy(request):
             db = client.bitcoin
             strategies = db.strategies
 
-            json_representation = int(form.cleaned_data['json_representation'])
+            json_representation = form.cleaned_data['json_representation']
             ast  = form.cleaned_data['ast']
             user_id = form.cleaned_data['user_id']
-            name = form.cleaned_data['name']
+            bot_name = form.cleaned_data['bot_name']
             frontend_graph = form.cleaned_data['frontend_graph']
-            strategies.update({'user_id': user_id, 'name' : name}, {'$set':  {'frontend_graph': frontend_graph, 'ast': ast, 'json_representation': json_representation}}, upsert=True)
+            strategies.update({'user_id': user_id, 'bot_name' : bot_name}, {'$set':  {'frontend_graph': frontend_graph, 'ast': ast, 'json_representation': json_representation}}, upsert=True)
 
-            return JsonResponse(data, safe=False)
+            return JsonResponse({"message": "Success"}, safe=False)
+    return JsonResponse({"message": "Error"}, safe=False)
